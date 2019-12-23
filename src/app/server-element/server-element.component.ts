caa2604ec117b02fe9dcbc11bd0a8187a7aa385c
alec wang <alex0123321@gmail.com>
2019-12-23 11:01:08 +0800

    finish from course

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange, OnChanges, SimpleChanges, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
 	selector: 'app-server-element',
 	templateUrl: './server-element.component.html',
 	styleUrls: [ './server-element.component.css' ]
 })
-export class ServerElementComponent implements OnInit {
-	@Input('srvElement') element: { type: string; name: string; content: string };
-	constructor() {}
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input('srvElement') element: { type: string; name: string; content: string };
+  @ContentChild('contentParagraph', { static: true}) paragraph: ElementRef;
+  constructor() {
+    console.log('constructor called');
+  }
 
-	ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+
+	ngOnInit() {
+    console.log('ngOnInit called');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentInit(){
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
 }
